d9493a3303198e911901867cbcba85723c49891b
Jeremy Collins <jeremy.collins@optimityadvisors.com>
2017-07-28 14:52:58 +0100

    Added html-replace to reference minified CSS & JS in dist HTML

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var cleanCSS = require('gulp-clean-css');
 var uglify = require("gulp-uglify");
 var rename = require('gulp-rename');
+var htmlreplace = require('gulp-html-replace');
 
 gulp.task('copy-html', function() {
     return gulp.src('./src/*.html')
@@ -9,27 +10,42 @@ gulp.task('copy-html', function() {
 });
 
 gulp.task('minify-css', function() {
-  return gulp.src('src/*.css')
-    .pipe(cleanCSS())
-    //TODO: update references to .min in "dist" HTML
-    // https://www.npmjs.com/package/gulp-html-replace
-    // .pipe(rename({
-    //   suffix: '.min'
-    // }))
-    .pipe(gulp.dest('./dist'));
+    return gulp.src('src/*.css')
+      .pipe(cleanCSS())      
+      .pipe(rename({
+        suffix: '.min'
+      }))
+      .pipe(gulp.dest('./dist'));
 });
 
 gulp.task('minify-js', function () {
-    gulp.src('./src/*.js') 
-    .pipe(uglify())
-    // .pipe(rename({
-    //   suffix: '.min'
-    // }))
+    return gulp.src('./src/*.js') 
+      .pipe(uglify())
+      .pipe(rename({
+        suffix: '.min'
+      }))
+      .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('release-html', function() {
+  gulp.src('./src/index.html')
+    .pipe(htmlreplace({
+        'css': 'florida.min.css',
+        'js': 'florida.min.js'
+    }))
     .pipe(gulp.dest('./dist'));
 });
 
+// "watch" is probably more realistic for SASS / LESS compilation, rather
+// than minifying with every save
 gulp.task('watch', function () {
-  gulp.watch('./src/*.js', ['minify-js']);
-  gulp.watch('./src/*.css', ['minify-css']);
-  gulp.watch('./src/*.html', ['copy-html']);
+    gulp.watch('./src/*.js', ['minify-js']);
+    gulp.watch('./src/*.css', ['minify-css']);
+    //gulp.watch('./src/*.html', ['copy-html']);
+    gulp.watch('./src/*.html', ['release-html']);
+});
+
+gulp.task('build', function() {
+  //TODO: how to execute multiple tasks outside of a "watch"?
+  // This might involve dependencies, as by default the tasks will run async
 });
